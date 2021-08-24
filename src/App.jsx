d88644f20b252eaf8d7f23c868ac5439c2d1d66a
Rafael Barbosa <rafael.costa@m4u.com.br>
2021-08-24 00:20:48 -0300

    testando chamada na api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { useEffect, useState } from "react";
+
+import api from "./services/api";
 
 function App() {
+
+  const [phone, setPhone] = useState([]);
+
+  useEffect(() => {
+    api.get('phones').then(response => {
+      console.log(response.data);
+    })
+  }, []);
+
   return (
     <h1>Hello World</h1>
   );
